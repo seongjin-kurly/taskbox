ddc6106f9ac278db9c5e0a8d3b39e789c1551a84
seongjin-kim <seongjin.kim@kurlycorp.com>
2022-05-06 07:59:25 +0900

    화면 - decorators와 함께 컨텍스트를 제공하기

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,19 +1,33 @@
 // src/components/InboxScreen.stories.js
 
 import React from 'react';
-
+import { Provider } from 'react-redux';
+import { action } from '@storybook/addon-actions';
 import { PureInboxScreen } from './InboxScreen';
+import * as TaskListStories from './TaskList.stories';
 
-export default {
-  component: PureInboxScreen,
-  title: 'InboxScreen',
-};
+// A super-simple mock of a redux store
+const store = {
+    getState: () => {
+      return {
+        tasks: TaskListStories.Default.args.tasks,
+      };
+    },
+    subscribe: () => 0,
+    dispatch: action('dispatch'),
+  };
 
-const Template = (args) => <PureInboxScreen {...args} />;
+  export default {
+    component: PureInboxScreen,
+    decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+    title: 'InboxScreen',
+  };
 
-export const Default = Template.bind({});
+  const Template = (args) => <PureInboxScreen {...args} />;
 
-export const Error = Template.bind({});
-Error.args = {
-  error: 'Something',
-};
+  export const Default = Template.bind({});
+
+  export const Error = Template.bind({});
+  Error.args = {
+    error: 'Something',
+  };
